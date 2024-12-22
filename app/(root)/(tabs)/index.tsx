5d5286944672f651a7347aec47883eb8170b9bd0
Tyler Terry <tylerterry@Tylers-MacBook-Pro.local>
2024-12-22 12:05:13 -0500

    route setup

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -9,7 +10,10 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/explore">explore</Link>
+      <Link href="/profile">profile</Link>
+      <Link href="/properties/1">Property 1</Link>
     </View>
   );
 }
